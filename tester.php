72990654947c161a5a7bfbc2096c56fe065bac44
Conner Ow <connerow1115@gmail.com>
2020-10-01 10:25:51 -0500

    Update tester.php

--- tester.php
+++ tester.php
@@ -2,10 +2,10 @@
 ini_set("SMTP", "aspmx.l.google.com");
 ini_set("sendmail_from", "connerow1115@gmail.com");
 
-$message = "The mail message was sent with the following mail setting:\r\nSMTP = aspmx.l.google.com\r\nsmtp_port = 25\r\nsendmail_from = YourMail@address.com";
+$message = "The mail message was sent with the following mail setting:\r\nSMTP = aspmx.l.google.com\r\nsmtp_port = 25\r\nsendmail_from = connerow1115@address.com";
 
-$headers = "From: YOURMAIL@gmail.com";
+$headers = "From: connerow1115@gmail.com";
 
-mail("Sending@provider.com", "Testing", $message, $headers);
+mail("connerow1115@gmail.com", "Testing", $message, $headers);
 echo "Check your email now....&lt;BR/>";
 ?>
